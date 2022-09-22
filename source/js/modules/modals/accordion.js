cbbcecd5d729a3a9cd162324d63bd56c5f49d00e
Vladimir Lachugin <Vladimirla4ygin@yandex.ru>
2022-09-22 18:29:41 +0300

    Markup

--- source/js/modules/modals/accordion.js
+++ source/js/modules/modals/accordion.js
@@ -1,17 +1,15 @@
 export const accordion = () => {
-  const listItems = document.querySelectorAll('.footer__list');
-  const buttonClose = document.querySelectorAll('.footer__toggle');
+  const listItems1 = document.getElementById('listToggle1');
+  const buttonClose1 = document.getElementById('btnToggle1');
+  const listItems2 = document.getElementById('listToggle2');
+  const buttonClose2 = document.getElementById('btnToggle2');
 
-  for (const listItem of listItems) {
-    listItem.classList.remove('footer__list--no-js');
-  }
-
-  buttonClose.forEach((btn) => {
-    btn.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      for (const listItem of listItems) {
-        listItem.classList.toggle('footer__list--open');
-      }
-    });
+  buttonClose1.addEventListener('click', (e) => {
+    e.preventDefault();
+    listItems1.classList.toggle('footer__list--open');
+  });
+  buttonClose2.addEventListener('click', (b) => {
+    b.preventDefault();
+    listItems2.classList.toggle('footer__list--open');
   });
 };
